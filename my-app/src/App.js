922076744bd1f904dfc5a19ae3f9f2ab41ceb5e0
kalakhhata <ompatel2501@gmail.com>
2025-02-06 20:16:03 -0500

    Here I have created a mini project about rating star

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import '../src/components/accordian/index.css'
 import Accordian from './components/accordian';
 import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/* Accordian Component */}
       <Accordian />
+      {/*Random Color Component */}
       <RandomColor />
+      {/*Star Component*/}
+      <StarRating />
+
     </div>
   );
 }
---
+++ my-app/src/components/image-slider/index.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+export default function ImageSlider({ url, limit }) {
+  const [images, setImages] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  async function fetchImages(getUrl) {
+    try {
+    } catch (e) {
+      setErrorMsg(e.message);
+    }
+  }
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
+  return (
+    <>
+      <div className="container"></div>
+    </>
+  );
+}
